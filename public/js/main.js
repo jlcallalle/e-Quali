ef770b6b5e115b23bdee76f465a6b29d579818a7
Jorge <jlcallalle@gmail.com>
2019-09-19 12:27:59 -0500

    se genera codigo svg path y polygon de molares, premolares, caninos y incisivos

--- public/js/main.js
+++ public/js/main.js
@@ -12,28 +12,13 @@ var MyApp = {
             return false;
         });
     },
-    onlyLetters : function () {
-        $('.only-letters').on('keypress', function(event) {
-            var englishAlphabetAndWhiteSpace = /[A-Za-z- ]/g;
-            var arr_latin_character = [8,37,39,193,225,200,232,205,237,211,243,218,250,209,241];
-            var key = String.fromCharCode(event.which);
-            if ($.inArray(event.keyCode, arr_latin_character) >= 0 || englishAlphabetAndWhiteSpace.test(key)) {
-                return true;
-            }
-            return false;
-        });
-    },
 }
 
 $(function () {
-    console.log('helo world');
-
     if ($('.only-numbers').length) {
         MyApp.onlyNumbers();
     }
-
-    if ($('.only-letters').length) {
-        MyApp.onlyLetters();
+    if ($('.page-odontograma').length) {
+       console.log('enjoy odontogram');
     }
-
 });
