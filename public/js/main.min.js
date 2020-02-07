fb42944e91a1ddac8a92230484fa0a7135e1d15a
jorge <jlcallalle@gmail.com>
2020-02-07 10:12:24 -0500

    se agrega hallazgo espigo muñon

--- public/js/main.min.js
+++ public/js/main.min.js
@@ -240,6 +240,7 @@ var MyApp = {
 
       if ( $(contenidoOdontograma).hasClass(listaHallazgo.hallazgoEspigoMuñon) ) {
          $(this).parents("svg").parent().toggleClass("active-espigo-muñon");
+
      }
 
       if ( $(contenidoOdontograma).hasClass(listaHallazgo.hallazgoEspigoMuñonMalo) ) {
@@ -908,8 +909,8 @@ var MyApp = {
                           $(this).parents(".svg").find('[data-pos="oclusal"]').removeClass("disabledbutton");
                           $(this).parents(".diastema").find("img").remove();
                           $(this).parents(".flecha-extruida, .flecha-intruida, .giroversion").find("img").remove();
-                          $(this).parents(".svg").parent().removeClass("tipo-pulponomia tipo-pulponomia-malo active-tratamiento-pulpar-nino active-tratamiento-pulpar-nino-malo active-tratamiento-pulpar active-tratamiento-pulpar-malo active-superficie-desgastada-linea-top active-hallazgo-superficie-desgastada-izquierda   active-hallazgo-superficie-desgastada-derecha active-superficie-desgastada-linea-sub active-sellantes active-sellantes-malo selecflecha-extruida flecha-intruida giroversion diastema");
-                          $(this).parents(".svg").removeClass("active-extruida active-intruida active-hallazgo-giroversion active-diastema active-hallazgo-implante-dental active-hallazgo-implante-dental-malo active-hallazgo-transposicion active-hallazgo-fractura active-hallazgo-fractura-linea-sub active-hallazgo-fractura-diagonal-izquierda active-hallazgo-fractura-diagonal-derecha active-hallazgo-fractura-diagonal-izquierda-small active-hallazgo-fractura-diagonal-derecha-small active-hallazgo-fractura-raya-izquierda active-hallazgo-fractura-raya-derecha active-hallazgo-protesis-removible active-hallazgo-protesis-removible-malo active-hallazgo-ortodontico-fijo active-hallazgo-ortodontico-fijo-malo disabled-range");
+                          $(this).parents(".svg").parent().removeClass("active-espigo-muñon active-espigo-muñon-malo tipo-pulponomia tipo-pulponomia-malo active-tratamiento-pulpar-nino active-tratamiento-pulpar-nino-malo active-tratamiento-pulpar active-tratamiento-pulpar-malo active-superficie-desgastada-linea-top active-hallazgo-superficie-desgastada-izquierda   active-hallazgo-superficie-desgastada-derecha active-superficie-desgastada-linea-sub active-sellantes active-sellantes-malo selecflecha-extruida flecha-intruida giroversion diastema");
+                          $(this).parents(".svg").removeClass("active-hallazgo-espigo-muñon active-hallazgo-espigo-muñon-malo active-extruida active-intruida active-hallazgo-giroversion active-diastema active-hallazgo-implante-dental active-hallazgo-implante-dental-malo active-hallazgo-transposicion active-hallazgo-fractura active-hallazgo-fractura-linea-sub active-hallazgo-fractura-diagonal-izquierda active-hallazgo-fractura-diagonal-derecha active-hallazgo-fractura-diagonal-izquierda-small active-hallazgo-fractura-diagonal-derecha-small active-hallazgo-fractura-raya-izquierda active-hallazgo-fractura-raya-derecha active-hallazgo-protesis-removible active-hallazgo-protesis-removible-malo active-hallazgo-ortodontico-fijo active-hallazgo-ortodontico-fijo-malo disabled-range");
                           $("svg").removeClass("disabled-range");
                       }
                   });
